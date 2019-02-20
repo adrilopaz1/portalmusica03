d7c482ec524a9ecb1a0684a165e796df6c19af0c
adrilopaz1 <47173769+adrilopaz1@users.noreply.github.com>
2019-02-20 10:34:23 +0100

    Update config_music.php

--- config_music.php
+++ config_music.php
@@ -1,5 +1,5 @@
 <?php
-   define('DB_SERVER', 'localhost');
+   define('DB_SERVER', '10.131.37.19');
    define('DB_USERNAME', 'root');
    define('DB_PASSWORD', 'rootroot');
    define('DB_DATABASE', 'musica');
